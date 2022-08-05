2b98c737ce0017a702205f2551b4bef96066864e
Ushakov Michael <um.nix.user@gmail.com>
2022-08-05 23:19:31 +0500

    Finally for today: SMB clients

---
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher/Smb/ISmbService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wissance.MossbauerLab.Watcher.Web.Smb
+{
+    public interface ISmbService
+    {
+        Task<IList<string>> GetChildrenAsync(string parent);
+        Task<byte[]> ReadAsync();
+    }
+}
---
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher/Smb/Smb1Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wissance.MossbauerLab.Watcher.Web.Smb
+{
+    /// <summary>
+    ///    Service that allows to work with Smb V1 (https://github.com/TalAloni/SMBLibrary/blob/master/ClientExamples.md)
+    /// </summary>
+    public class Smb1Service : ISmbService
+    {
+        public Task<IList<string>> GetChildrenAsync(string parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> ReadAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
