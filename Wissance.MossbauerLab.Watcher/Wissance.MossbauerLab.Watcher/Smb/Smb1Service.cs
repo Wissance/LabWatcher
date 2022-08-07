053d9a4036ddd8535ce74972b4868d3607698ec2
Ushakov Michael <um.nix.user@gmail.com>
2022-08-08 01:11:16 +0500

    intermediate

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher/Smb/Smb1Service.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher/Smb/Smb1Service.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SMBLibrary;
+using SMBLibrary.Client;
+using Wissance.MossbauerLab.Watcher.Web.Config;
 
 namespace Wissance.MossbauerLab.Watcher.Web.Smb
 {
@@ -10,8 +15,41 @@ namespace Wissance.MossbauerLab.Watcher.Web.Smb
     /// </summary>
     public class Smb1Service : ISmbService
     {
-        public Task<IList<string>> GetChildrenAsync(string parent)
+        public Smb1Service(SmbConfig config, ILoggerFactory loggerFactory)
         {
+            _config = config;
+            _logger = loggerFactory.CreateLogger<Smb1Service>();
+        }
+
+        public async Task<IList<string>> GetChildrenAsync(string parent)
+        {
+            try
+            {
+                IList<string> children = new List<string>();
+                SMB1Client client = new SMB1Client();
+                bool isConnected = client.Connect(IPAddress.Parse(_config.Address), SMBTransportType.DirectTCPTransport);
+                if (isConnected)
+                {
+                    bool hasPassword = _config.UserCredentials != null;
+                    NTStatus status = client.Login(String.Empty, hasPassword ? _config.UserCredentials.User : String.Empty,
+                                                   hasPassword ? _config.UserCredentials.Password : String.Empty);
+                    if (status == NTStatus.STATUS_SUCCESS)
+                    {
+                        List<string> shares = client.ListShares(out status);
+                        // todo ...
+                        client.Logoff();
+                    }
+                    client.Disconnect();
+                }
+                return children;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            //client.Connect()
             throw new NotImplementedException();
         }
 
@@ -19,5 +57,8 @@ namespace Wissance.MossbauerLab.Watcher.Web.Smb
         {
             throw new NotImplementedException();
         }
+
+        private readonly SmbConfig _config;
+        private readonly ILogger<Smb1Service> _logger;
     }
 }
