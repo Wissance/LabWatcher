b3c32a45024fdf95a90dce44b99be8982c3e6a22
Ushakov Michael <um.nix.user@gmail.com>
2022-08-14 23:49:33 +0500

    used new lib

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher/Store/NetworkFileSystemStoreService.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher/Store/NetworkFileSystemStoreService.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Win32.SafeHandles;
+using SimpleImpersonation;
 using Wissance.MossbauerLab.Watcher.Web.Config;
+using UserCredentials = SimpleImpersonation.UserCredentials;
 
 namespace Wissance.MossbauerLab.Watcher.Web.Store
 {
@@ -11,12 +16,32 @@ namespace Wissance.MossbauerLab.Watcher.Web.Store
     {
         public NetworkFileSystemStoreService(SpectraStoreConfig config, ILoggerFactory loggerFactory)
         {
-
+            _config = config;
+            _logger = loggerFactory.CreateLogger<NetworkFileSystemStoreService>();
         }
 
-        public Task<IList<string>> GetChildrenAsync(string shareName, string parent)
+        public async Task<IList<string>> GetChildrenAsync(string shareName, string parent)
         {
-            throw new NotImplementedException();
+            try
+            {
+                UserCredentials credentials = GetCredentials();
+                using (SafeAccessTokenHandle userHandle = credentials.LogonUser(LogonType.Interactive))
+                {
+                    IList<string> children = await WindowsIdentity.RunImpersonatedAsync(userHandle, async () =>
+                    {
+                        return System.IO.Directory.GetFiles($@"\\{_config.Address}\{shareName}").ToList();
+                    });
+                    // todo: umv: implement filtering
+                    return children;
+                }
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred during getting children items of shared folder: {e.Message}");
+                return null;
+            }
+
         }
 
         public Task<byte[]> ReadAsync()
@@ -24,7 +49,14 @@ namespace Wissance.MossbauerLab.Watcher.Web.Store
             throw new NotImplementedException();
         }
 
+        private UserCredentials GetCredentials()
+        {
+            if (_config.UserCredentials == null)
+                return null;
+            return new UserCredentials(_config.UserCredentials.User, _config.UserCredentials.Password, _config.Domain);
+        }
+
         private readonly SpectraStoreConfig _config;
-        private readonly ILogger<Smb1Service> _logger;
+        private readonly ILogger<NetworkFileSystemStoreService> _logger;
     }
 }
