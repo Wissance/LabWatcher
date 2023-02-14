4a578a3dce120cf35b836b27edf861bbe41d87fe
Ushakov Michael <um.nix.user@gmail.com>
2023-02-14 23:49:22 +0500

    path changed

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Tests/TelegramNotificationsTests.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Tests/TelegramNotificationsTests.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Wissance.MossbauerLab.Watcher.Common.Data.Notification;
 
-namespace Wissance.MossbauerLab.Watcher.Tests
+namespace Wissance.MossbauerLab.Watcher.Services.Tests
 {
     [TestClass]
     public class TelegramNotificationsTests
