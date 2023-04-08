9603eb35e506a161d7883af1863d7d2f644ca3c2
Ushakov Michael <um.nix.user@gmail.com>
2023-04-08 14:41:03 +0500

    appsettings config changes apply

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Config/TemplatesConfig.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Config/TemplatesConfig.cs
@@ -5,7 +5,20 @@ using System.Threading.Tasks;
 
 namespace Wissance.MossbauerLab.Watcher.Web.Config
 {
+    public class MailTemplates
+    {
+        public string AutosaveDone { get; set; }
+    }
+
+    public class TelegramTemplates
+    {
+        public string AutosaveDone { get; set; }
+        public string AutosaveEmpty { get; set; }
+    }
+
     public class TemplatesConfig
     {
+        public MailTemplates Mail { get; set; }
+        public TelegramTemplates Telegram { get; set; }
     }
 }
