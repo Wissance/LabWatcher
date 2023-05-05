1f91fa826109f33e2b240f1c6adfe2b62247c014
Ushakov Michael <um.nix.user@gmail.com>
2023-05-05 23:14:05 +0500

    config re-configuration

---
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Common/Data/Storage/FtpRequisites.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wissance.MossbauerLab.Watcher.Common.Data.Storage
+{
+    public class FtpRequisites
+    {
+        public FtpRequisites()
+        {
+        }
+
+        public FtpRequisites(string host, int port, string username, string password)
+        {
+            Host = host;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
