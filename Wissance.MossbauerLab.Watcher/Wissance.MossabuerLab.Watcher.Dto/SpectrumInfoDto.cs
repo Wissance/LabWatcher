f794430b530a2e7e7d1a0a36760fdd1dfd529073
Ushakov Michael <um.nix.user@gmail.com>
2023-05-08 22:47:21 +0500

    Added Dto project, Toolkit updated to 1.1.2, Factory && Manager 4 SpectrumEntity

---
+++ Wissance.MossbauerLab.Watcher/Wissance.MossabuerLab.Watcher.Dto/SpectrumInfoDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wissance.MossabuerLab.Watcher.Dto
+{
+    public class SpectrumInfoDto
+    {
+        public SpectrumInfoDto()
+        {
+        }
+
+        public SpectrumInfoDto(int id, string name, string description, DateTime measureStartDate, DateTime? first, DateTime? last, bool isArchived)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            MeasureStartDate = measureStartDate;
+            First = first;
+            Last = last;
+            IsArchived = isArchived;
+        }
+
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime MeasureStartDate { get; set; }
+        public DateTime? First { get; set; }
+        public DateTime? Last { get; set; }
+        public bool IsArchived { get; set; }
+    }
+}
---
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Managers/SpectrumManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Wissance.MossabuerLab.Watcher.Dto;
+using Wissance.MossbauerLab.Watcher.Data.Entities;
+using Wissance.WebApiToolkit.Managers;
+
+namespace Wissance.MossbauerLab.Watcher.Web.Managers
+{
+    public class SpectrumManager: EfModelManager<SpectrumEntity, SpectrumInfoDto, int>
+    {
+        public SpectrumManager(DbContext dbContext, Func<SpectrumEntity, IDictionary<string, string>, bool> filterFunc, Func<SpectrumEntity, SpectrumInfoDto> createFunc, ILoggerFactory loggerFactory) 
+            : base(dbContext, filterFunc, createFunc, loggerFactory)
+        {
+        }
+    }
+}
