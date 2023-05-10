ff95da88bff4cd3ce1b92ee81d00a0377d2a723f
Ushakov Michael <um.nix.user@gmail.com>
2023-05-10 10:27:48 +0500

    getting samples method

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Managers/SpectrumManager.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Managers/SpectrumManager.cs
@@ -25,6 +25,7 @@ namespace Wissance.MossbauerLab.Watcher.Web.Managers
             _context = dbContext;
             _storeService = storeService;
             _appConfig = appConfig;
+            _logger = loggerFactory.CreateLogger<SpectrumManager>();
         }
 
         public async Task<OperationResultDto<SpectrumSamplesInfoDto>> GetSpectrumSamplesAsync(int spectrumId)
@@ -33,20 +34,59 @@ namespace Wissance.MossbauerLab.Watcher.Web.Managers
             {
                 SpectrumEntity spectrum = await _context.Spectra.FirstOrDefaultAsync(s => s.Id == spectrumId);
                 if (spectrum == null)
+                {
+                    string msg = $"Spectrum with id: {spectrumId} was not found";
+                    _logger.LogDebug(msg);
                     return new OperationResultDto<SpectrumSamplesInfoDto>(false, (int)HttpStatusCode.NotFound, $"Spectrum with id: {spectrumId} was not found", null);
+                }
+
                 string[] samples = await GetSamples(spectrum);
                 return new OperationResultDto<SpectrumSamplesInfoDto>(true, (int)HttpStatusCode.OK, null, SpectrumSampleFactory.Create(spectrum, samples));
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
-                throw;
+                string msg = $"An error occurred during getting spectrum {spectrumId} samples, error: {e.Message}";
+                _logger.LogError(msg);
+                return new OperationResultDto<SpectrumSamplesInfoDto>(true, (int)HttpStatusCode.InternalServerError, msg, null);
             }
         }
 
-        public async Task<OperationResultDto<byte[]>> GetSpectrumSampleFileAsync(int spectrum, string sampleNumber)
+        public async Task<OperationResultDto<byte[]>> GetSpectrumSampleFileAsync(int spectrumId, string sampleNumber)
         {
-            return null;
+            try
+            {
+                SpectrumEntity spectrum = await _context.Spectra.FirstOrDefaultAsync(s => s.Id == spectrumId);
+                if (spectrum == null)
+                {
+                    string msg = $"Spectrum with id: {spectrumId} was not found";
+                    _logger.LogDebug(msg);
+                    return new OperationResultDto<byte[]>(false, (int)HttpStatusCode.NotFound, msg, null);
+                }
+
+                string[] samples = await GetSamples(spectrum);
+                if (samples == null || samples.Length == 0)
+                {
+                    _logger.LogWarning("There are no samples, it looks suspicious maybe shared folder or FTP are inaccessible? ");
+                    return new OperationResultDto<byte[]>(false, (int)HttpStatusCode.OK, $"Samples are empty", null);
+                }
+
+                string sampleFile = samples.FirstOrDefault(s => s.ToLower().Contains(sampleNumber.ToLower()));
+                if (sampleFile == null)
+                {
+                    string msg = $"There are no sample {sampleNumber} for spectrum with id: {spectrumId}";
+                    _logger.LogDebug(msg);
+                    return new OperationResultDto<byte[]>(false, (int)HttpStatusCode.NotFound, msg, null);
+                }
+
+                byte[] spectrumSampleContent = await ReadSpectrumFile(spectrum, sampleFile);
+                return new OperationResultDto<byte[]>(true, (int)HttpStatusCode.OK, null, spectrumSampleContent);
+            }
+            catch (Exception e)
+            {
+                string msg = $"An error occurred during getting spectrum {spectrumId} sample \"{sampleNumber}\", error: {e.Message}";
+                _logger.LogError(msg);
+                return new OperationResultDto<byte[]>(true, (int)HttpStatusCode.InternalServerError, msg, null);
+            }
         }
 
         private async Task<string[]> GetSamples(SpectrumEntity spectrum)
@@ -63,6 +103,22 @@ namespace Wissance.MossbauerLab.Watcher.Web.Managers
             }
         }
 
+        private async Task<byte[]> ReadSpectrumFile(SpectrumEntity spectrum, string sampleFile)
+        {
+            if (!spectrum.IsArchived)
+            {
+                string fullPath = Path.Combine(spectrum.Location, sampleFile);
+                byte[] spectrumSampleContent = await _storeService.ReadAsync(fullPath);
+                return spectrumSampleContent;
+            }
+            else
+            {
+                // we should get from FTP, temporarily we can't do (FTP was not configured properly and we still in DEBUG)
+                return null;
+            }
+        }
+
+        private readonly ILogger<SpectrumManager> _logger;
         private readonly IModelContext _context;
         private readonly IFileStoreService _storeService;
         private readonly ApplicationConfig _appConfig;
