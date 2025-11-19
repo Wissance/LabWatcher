0aee9c0cca605d4698747aba15a77228509b561a
Ushakov Michale <um.nix.user@gmail.com>
2025-11-19 18:52:50 +0500

    Spectrum details answer preliminary is ready

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Command/GetSpectrumDetailsCommand.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Command/GetSpectrumDetailsCommand.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
 using Wissance.MossbauerLab.Watcher.Dto;
 using Wissance.MossbauerLab.Watcher.Web.Managers;
 using Wissance.MossbauerLab.Watcher.Web.Services.Store;
@@ -41,14 +44,40 @@ namespace Wissance.MossbauerLab.Watcher.Web.Command
                     return false;
                 }
 
-                OperationResultDto<SpectrumSamplesInfoDto> spectrumInfo = await manager.GetSpectrumSamplesAsync(spectrumId);
-                if (!spectrumInfo.Success)
+                OperationResultDto <SpectrumInfoDto> spectrum = await manager.GetByIdAsync(spectrumId);
+                if (!spectrum.Success)
                 {
-                    _logger.LogError($"Error occurred during getting Spectrum samples in \"GetSpectrumDetailsCommand\" : {spectrumInfo.Message}");
+                    _logger.LogError($"Error occurred during getting Spectrum by id in \"GetSpectrumDetailsCommand\" : {spectrum.Message}");
                     await _context.BotClient.SendTextMessageAsync(_context.RawMessage.Chat.Id, CommandAnswerLocalizationDefs.UnknownError);
                     return false;
                 }
-                // todo(UMV): form answer
+                
+                OperationResultDto<SpectrumSamplesInfoDto> spectrumSamples = await manager.GetSpectrumSamplesAsync(spectrumId);
+                if (!spectrumSamples.Success)
+                {
+                    _logger.LogError($"Error occurred during getting Spectrum samples in \"GetSpectrumDetailsCommand\" : {spectrumSamples.Message}");
+                    await _context.BotClient.SendTextMessageAsync(_context.RawMessage.Chat.Id, CommandAnswerLocalizationDefs.UnknownError);
+                    return false;
+                }
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append("```");
+                sb.Append(string.Format(AnswerTemplate, spectrumId, spectrum.Data.Name, 
+                    1,
+                    spectrum.Data.MeasureStartDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                    spectrum.Data.First.HasValue ? spectrum.Data.First?.ToString("yyyy-MM-dd HH:mm:ss") : "",
+                    spectrum.Data.Last.HasValue ? spectrum.Data.Last?.ToString("yyyy-MM-dd HH:mm:ss") : "",
+                    CommandAnswerLocalizationDefs.GetArchivedCaption(spectrum.Data.IsArchived)
+                    ));
+                int n = 1;
+                foreach (string sample in spectrumSamples.Data.Samples)
+                {
+                    sb.Append($"  - {n}. {sample}");
+                    n++;
+                }
+                sb.Append("```");
+                await _context.BotClient.SendTextMessageAsync(_context.RawMessage.Chat.Id, sb.ToString(),
+                    ParseMode.Markdown);
                 return true;
             }
             catch (Exception e)
@@ -57,6 +86,15 @@ namespace Wissance.MossbauerLab.Watcher.Web.Command
                 return false;
             }
         }
+
+        private const string AnswerTemplate = "* id спетра : {0}\n   " +
+                                              "* имя спектра : *{1}*\n   " +
+                                              "* канал: {2}\n   " +
+                                              "* дата начала измерения : `{3}`\n   " +
+                                              "* первый сохраненный файл : `{4}`\n   " +
+                                              "* дата окончания измерения : `{5}`\n   " +
+                                              "* статус : {6}\n   " +
+                                              "* список файлов:";
         
         private readonly CommandContext _context;
         private readonly ILogger< GetSpectrumDetailsCommand> _logger;
