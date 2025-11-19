f9e72385feff678b62a88c8438e108b2e8a4819a
Ushakov Michale <um.nix.user@gmail.com>
2025-11-19 18:31:37 +0500

    Spectrum details cmd answer

--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Command/CommandAnswerLocalizationDefs.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Command/CommandAnswerLocalizationDefs.cs
@@ -18,6 +18,9 @@ namespace Wissance.MossbauerLab.Watcher.Web.Command
 
         public const string UnknownError = "Произошла ошибка, свяжитесь с администратором систем";
 
+        public const string SpectrumIdNotProvidedError = "Для выполнения этой команды необходимо предоставить целочисленный идентификатор спектра";
+        public const string SpectrumIdCantBeExtractedError = "Значение переданное в качестве идентификатора спектра не является целочисленным";
+        
         public static readonly IDictionary<string, string> KeyboardCaptions = new Dictionary<string, string>()
         {
             {ListSpectraCmd, "Вывести список спектров"},
--- Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Command/GetSpectrumDetailsCommand.cs
+++ Wissance.MossbauerLab.Watcher/Wissance.MossbauerLab.Watcher.Web/Command/GetSpectrumDetailsCommand.cs
@@ -1,13 +1,64 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Wissance.MossbauerLab.Watcher.Dto;
+using Wissance.MossbauerLab.Watcher.Web.Managers;
+using Wissance.MossbauerLab.Watcher.Web.Services.Store;
+using Wissance.WebApiToolkit.Dto;
 
 namespace Wissance.MossbauerLab.Watcher.Web.Command
 {
     public class GetSpectrumDetailsCommand : ICommand
     {
+        public GetSpectrumDetailsCommand(CommandContext context)
+        {
+            _context = context;
+            _logger = _context.LoggerFactory.CreateLogger<GetSpectrumDetailsCommand>();
+        }
+
         public async Task<bool> ExecuteAsync(string[] parameters)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (parameters.Length < 1)
+                {
+                    await _context.BotClient.SendTextMessageAsync(_context.RawMessage.Chat.Id,
+                        CommandAnswerLocalizationDefs.SpectrumIdNotProvidedError);
+                    return false;
+                }
+
+                SpectrumManager manager = new SpectrumManager(_context.Context, _context.LoggerFactory, 
+                    new WindowsShareStoreService(_context.Config.Sm2201SpectraStoreSettings, _context.LoggerFactory), _context.Config);
+                int spectrumId = -1;
+                bool parseRes = Int32.TryParse(parameters[0], out spectrumId);
+                if (!parseRes)
+                {
+                    _logger.LogError($"Provided as id value \"{parameters[0]}\" can't be parsed to int");
+                    await _context.BotClient.SendTextMessageAsync(_context.RawMessage.Chat.Id,
+                        CommandAnswerLocalizationDefs.SpectrumIdCantBeExtractedError);
+                    return false;
+                }
+
+                OperationResultDto<SpectrumSamplesInfoDto> spectrumInfo = await manager.GetSpectrumSamplesAsync(spectrumId);
+                if (!spectrumInfo.Success)
+                {
+                    _logger.LogError($"Error occurred during getting Spectrum samples in \"GetSpectrumDetailsCommand\" : {spectrumInfo.Message}");
+                    await _context.BotClient.SendTextMessageAsync(_context.RawMessage.Chat.Id, CommandAnswerLocalizationDefs.UnknownError);
+                    return false;
+                }
+                // todo(UMV): form answer
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"An error occurred during the handling {_context.Command} command, {e.Message}");
+                return false;
+            }
         }
+        
+        private readonly CommandContext _context;
+        private readonly ILogger< GetSpectrumDetailsCommand> _logger;
     }
 }
